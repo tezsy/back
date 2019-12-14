0573bcba7247c2773b917427885f51f46886f754
tezsy <alifzulkifeli@gmail.com>
2019-12-14 23:20:53 +0900

    connect

--- controllers/mercari.js
+++ controllers/mercari.js
@@ -3,45 +3,13 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 var request = require("request");
 
-exports.listSearch =  (req, res) => {
-    const item = [];
-    const price = '4';
-    getvals().then(price => { 
-
-    fetch(`https://www.mercari.com/jp/search/?page=${req.query.page||1}&keyword=${req.query.search}&price_min=${req.query.min_price || ""}&price_max=${req.query.max_price || ""}`)
-
-      .then(res => res.text())
-      .then(body => {
-        
-        const $ = cheerio.load(body,
-          {
-            withDomLvl1: true,
-            normalizeWhitespace: false,
-            xmlMode: true,
-            decodeEntities: true
-          }
-        );
-        $('.items-box a').each((i,el) => {
-
-          const mercari_item = {
-            name : $(el).find('.items-box-name').text(),
-            // price : parseInt($(el).find('.items-box-price').text().substr(1)) ,
-            price : parseFloat($(el).find('.items-box-price').text().substr(1).replace(/,/g, '')) * parseFloat(price || 3.8),
-            link : $(el).attr('href'),
-            image1 : $(el).children().children().attr('data-src'),
-            description : ''
-          }
-        
-          mercari_item.price = (Math.round(mercari_item.price * 100) / 100).toFixed(2);
-
-          item.push(mercari_item);
+exports.listSearch =  async (req, res) => {
+  await fetch(`https://jombeli.ngrok.io/pi/mercari/search?page=${req.query.page||1}&search=${req.query.search}&price_min=${req.query.min_price || ""}&price_max=${req.query.max_price || ""}`)
   
-        });
-        res.json(item);
-        
+      .then(res => res.json())
+      .then(body => {
+        res.json(body);
       });
-    });
-
     };
 
 
